6bca5faaf465cdfcc15e6ce0ed3d909af4b198dc
Scott Wakefield <scott@clubstudio.co.uk>
2015-04-11 07:37:45 +0000

    Update code style to meet P&T standards

--- assetrev/AssetRevPlugin.php
+++ assetrev/AssetRevPlugin.php
@@ -3,49 +3,49 @@ namespace Craft;
 
 class AssetRevPlugin extends BasePlugin
 {
-    public function getName()
-    {
-        return 'Club - Asset Rev';
-    }
+	public function getName()
+	{
+		return 'Club - Asset Rev';
+	}
 
-    /**
-     * Returns the plugin’s version number.
-     *
-     * @return string The plugin’s version number.
-     */
-    public function getVersion()
-    {
-        return '1.0.0';
-    }
+	/**
+	 * Returns the plugin’s version number.
+	 *
+	 * @return string The plugin’s version number.
+	 */
+	public function getVersion()
+	{
+		return '1.0.0';
+	}
 
-    /**
-     * Returns the plugin developer’s name.
-     *
-     * @return string The plugin developer’s name.
-     */
-    public function getDeveloper()
-    {
-        return 'Club Studio Ltd';
-    }
+	/**
+	 * Returns the plugin developer’s name.
+	 *
+	 * @return string The plugin developer’s name.
+	 */
+	public function getDeveloper()
+	{
+		return 'Club Studio Ltd';
+	}
 
-    /**
-     * Returns the plugin developer’s URL.
-     *
-     * @return string The plugin developer’s URL.
-     */
-    public function getDeveloperUrl()
-    {
-        return 'https://clubstudio.co.uk';
-    }
+	/**
+	 * Returns the plugin developer’s URL.
+	 *
+	 * @return string The plugin developer’s URL.
+	 */
+	public function getDeveloperUrl()
+	{
+		return 'https://clubstudio.co.uk';
+	}
 
-    /**
-     * @return AssetRevTwigExtension
-     * @throws \Exception
-     */
-    public function addTwigExtension()
-    {
-        Craft::import('plugins.assetrev.twigextensions.AssetRevTwigExtension');
+	/**
+	 * @return AssetRevTwigExtension
+	 * @throws \Exception
+	 */
+	public function addTwigExtension()
+	{
+		Craft::import('plugins.assetrev.twigextensions.AssetRevTwigExtension');
 
-        return new AssetRevTwigExtension();
-    }
+		return new AssetRevTwigExtension();
+	}
 }
